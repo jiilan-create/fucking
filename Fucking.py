35221ec8bf84bd3ac22bb1c6a49d83a64850091d
jiilan-create <66149604+jiilan-create@users.noreply.github.com>
2020-06-06 11:24:09 +0700

    Add files via upload

---
+++ Fucking.py
@@ -0,0 +1,21 @@
+import sys,time
+def jalan (kata):
+    for e in kata:
+       sys.stdout.write(e)
+       sys.stdout.flush()
+       time.sleep(0.1)
+jalan("Goblok lu beraninya keroyokan dan dibelakang".center(44))
+jalan("nih buat lu bansat".center(44))
+jalan("             ".center(44))
+jalan("
+                      
+…………………../´¯/
+……………….../¯../
+………………../…./
+…………./´¯/’…’/´¯¯`·¸
+………./’/…/…./……./¨¯\
+……..///…´…´…. ¯~/’…’/
+………\……………..’…../
+…….…\………..... _.·´
+…………\…………..\
+…………..\………….\… ".center(44))
